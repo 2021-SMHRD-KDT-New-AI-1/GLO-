1fc1a36b377bc686d6502af4560e2a298be55fef
isco <73702085+iscoleesco@users.noreply.github.com>
2021-12-04 12:20:12 +0900

    Update recommandation
    more btn scroll

--- GLO--master/js/reco.js
+++ GLO--master/js/reco.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // more__recos btn click scrolling
-const moreRecoBtn = document.querySelector(".more__recos");
+const moreBtn = document.querySelector(".more__recos");
 
-moreRecoBtn.addEventListener("click", (event) => {
+moreBtn.addEventListener("click", (event) => {
     const target = event.target;
     const link = target.dataset.link
     const scrollTo = document.querySelector(link);
     scrollTo.scrollIntoView({behavior: "smooth"});
 });
+
