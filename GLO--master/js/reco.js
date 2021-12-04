164e428a70c7e51ca5248d3fd622e6ade470520e
isco <73702085+iscoleesco@users.noreply.github.com>
2021-12-04 14:47:27 +0900

    Update recommandation
    small navbar click scroll

--- GLO--master/js/reco.js
+++ GLO--master/js/reco.js
@@ -1,5 +1,22 @@
 "use strict";
 
+// small__navbar click scrolling
+const listVideo = document.querySelector(".list.video"),
+    listMusic = document.querySelector(".list.music");
+
+listVideo.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+listMusic.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
 // more__recos btn click scrolling
 const moreBtn = document.querySelector(".more__recos");
 
